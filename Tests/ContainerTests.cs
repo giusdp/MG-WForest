c966f3ef42bda35e583155750c17878956ac25b7
giusdp <depalma.gsp@gmail.com>
2020-12-01 12:35:51 +0100

    Remove test

--- Tests/ContainerTests.cs
+++ Tests/ContainerTests.cs
@@ -9,21 +9,11 @@ namespace WForest.Tests
     public class ContainerTests
     {
         [Test]
-        public void NewContainer_StartsAtSizeZero()
+        public void SimpleContainer_StartsAtSizeZero()
         {
             var container = Widgets.Container();
             Assert.That(container.Space, Is.EqualTo(Rectangle.Empty));
         }
 
-        // [Test]
-        // public void ContainerWithChild_AsBigAsChild()
-        // {
-        //     var container = Widgets.Container();
-        //     var tree = new WidgetTree(container);
-        //     tree.AddChild(Widgets.Container(50, 60));
-        //     
-        //     tree.ApplyProperties();
-        //     Assert.That(container.Space.Size, Is.EqualTo(new Point(50, 60)));
-        // }
     }
 }
