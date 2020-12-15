2ad2d771116753e29bb96d860edcd5acc55ac650
giusdp <depalma.gsp@gmail.com>
2020-12-15 17:36:15 +0100

    Add parent size checking to not let widgets go out of bound when dragging

--- src/UI/Properties/Draggable.cs
+++ src/UI/Properties/Draggable.cs
@@ -43,15 +43,26 @@ namespace WForest.UI.Properties
                     var (devX, devY) = devLoc;
                     var (x, y, w, h) = widgetNode.Data.Space;
                     if (dragCtx.DevX == devX && dragCtx.DevY == devY) return;
-                    var ddx = devX - dragCtx.DevX;
-                    var ddy = devY - dragCtx.DevY;
                     var nx = x + (devX - dragCtx.DevX);
                     var ny = y + (devY - dragCtx.DevY);
+
+                    if (widgetNode.Parent != null)
+                    {
+                        if (nx + w > widgetNode.Parent.Data.Space.X + widgetNode.Parent.Data.Space.Width)
+                            nx = widgetNode.Parent.Data.Space.Width - w;
+                        else if (nx < widgetNode.Parent.Data.Space.X) nx = widgetNode.Parent.Data.Space.X;
+
+                        if (ny + h > widgetNode.Parent.Data.Space.Y + widgetNode.Parent.Data.Space.Height)
+                            ny = widgetNode.Parent.Data.Space.Height - h;
+                        else if (ny < widgetNode.Parent.Data.Space.Y)
+                            ny = widgetNode.Parent.Data.Space.Y;
+                    }
+
                     WidgetsSpaceHelper.UpdateSpace(widgetNode, new Rectangle(nx, ny, w, h));
                     dragCtx.Set(devLoc);
                 }
             });
-            
+
             widgetNode.Data.AddOnRelease(() => dragCtx.IsDragging = false);
             widgetNode.Data.AddOnExit(() => dragCtx.IsDragging = false);
         }
