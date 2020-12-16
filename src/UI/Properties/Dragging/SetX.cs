ec51ed6c23ce60f3367f5f12ea59187f433db99f
giusdp <depalma.gsp@gmail.com>
2020-12-16 12:26:31 +0100

    Create Dragging folder, move Draggable.cs in it and create SetX and SetY props

---
+++ src/UI/Properties/Dragging/SetX.cs
@@ -0,0 +1,10 @@
+namespace WForest.UI.Properties.Dragging
+{
+    public class SetX : Property
+    {
+        internal override void ApplyOn(WidgetTree.WidgetTree widgetNode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/UI/Properties/Dragging/SetY.cs
@@ -0,0 +1,10 @@
+namespace WForest.UI.Properties.Dragging
+{
+    public class SetY : Property
+    {
+        internal override void ApplyOn(WidgetTree.WidgetTree widgetNode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
