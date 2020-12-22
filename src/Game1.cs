a87c939bc8461992caa85554ce227e1f79664026
giusdp <depalma.gsp@gmail.com>
2020-12-22 13:00:06 +0100

    Test fontstashsharp library

--- src/Game1.cs
+++ src/Game1.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.IO;
+using FontStashSharp;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Serilog;
 using WForest.UI;
 
 namespace WForest
@@ -10,6 +13,7 @@ namespace WForest
         private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private HUD _hud;
+        private FontSystem _fontSystem;
 
         public Game1()
         {
@@ -24,12 +28,16 @@ namespace WForest
             _graphics.PreferredBackBufferWidth = 1280;
             _graphics.PreferredBackBufferHeight = 720;
             _graphics.ApplyChanges();
+            Log.Information("Before run?");
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             AssetLoader.Initialize(Content);
+            _fontSystem = FontSystemFactory.Create(GraphicsDevice, 1280, 720);
+            Log.Debug($"{Content.RootDirectory}");
+            _fontSystem.AddFont(File.ReadAllBytes("Fonts/Comfortaa-Regular.ttf"));
             _hud = new HUD();
         }
 
@@ -49,8 +57,12 @@ namespace WForest
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp);
             _hud.Draw(_spriteBatch);
+            DynamicSpriteFont font18 = _fontSystem.GetFont(18);
+            _spriteBatch.DrawString(font18, "The quick brown\nfox jumps over\nthe lazy dog",
+                new Vector2(0, 0), Color.White);
+
             _spriteBatch.End();
-            
+
             base.Draw(gameTime);
         }
     }
