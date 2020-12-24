ec9e0923248c8919553686e7b7bcb70da8f04447
giusdp <depalma.gsp@gmail.com>
2020-12-24 15:37:16 +0100

    Remove draw text in font and add getter for the spritefont

--- src/UI/Widgets/TextWidget/Font.cs
+++ src/UI/Widgets/TextWidget/Font.cs
@@ -14,22 +14,21 @@ namespace WForest.UI.Widgets.TextWidget
 
         public Font(FontSystem fontSystem, int fontSize = 12)
         {
-            _fontSystem = fontSystem; 
+            _fontSystem = fontSystem;
             Size = fontSize;
         }
 
-        public void DrawText(SpriteBatch spriteBatch, string text)
-        {
-            spriteBatch.DrawString(_fontSystem.GetFont(Size), text, new Vector2(0, 0), Color.White);
-        }
-
-        private static FontSystem CreateFont(GraphicsDevice graphicsDevice, List<string> fontFiles,
+        public Font(GraphicsDevice graphicsDevice, List<string> fontFiles,
             int textureWidth = 1024,
-            int textureHeight = 1024)
+            int textureHeight = 1024, int fontSize = 12)
         {
             var fontSystem = FontSystemFactory.Create(graphicsDevice, textureWidth, textureHeight);
             fontFiles.ForEach(f => fontSystem.AddFont(File.ReadAllBytes(f)));
-            return fontSystem;
+            _fontSystem = fontSystem;
+            Size = fontSize;
         }
+
+        public DynamicSpriteFont SpriteFont() => _fontSystem.GetFont(Size);
+
     }
 }
