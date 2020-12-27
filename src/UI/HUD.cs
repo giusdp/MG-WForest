4697fd421dc88e803bbae5e85d89164da121d430
giusdp <depalma.gsp@gmail.com>
2020-12-27 17:07:21 +0100

    Add resize in HUD to update widget tree after resize event

--- src/UI/HUD.cs
+++ src/UI/HUD.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Serilog;
 using WForest.UI.Properties.Text;
+using WForest.UI.Utils;
 using WForest.UI.Widgets.TextWidget;
 using WForest.UI.WidgetTree;
 
@@ -12,13 +13,12 @@ namespace WForest.UI
         private readonly WidgetTree.WidgetTree _root;
         private readonly WidgetTreeVisitor _widgetTreeVisitor;
 
-
-        public HUD()
+        public HUD(int x, int y, int width, int height)
         {
             _widgetTreeVisitor = new WidgetTreeVisitor();
 
-            _root = new WidgetTree.WidgetTree(Factories.Widgets.Container(new Rectangle(0, 0, 1280, 720)));
-            _root.AddProperty(Factories.Properties.Row());
+            _root = new WidgetTree.WidgetTree(Factories.Widgets.Container(width, height));
+            _root.AddProperty(Factories.Properties.Column());
             _root.AddProperty(Factories.Properties.JustifyCenter());
             _root.AddProperty(Factories.Properties.ItemCenter());
 
@@ -31,15 +31,23 @@ namespace WForest.UI
             var textW2 = new Text("Test 2 TextWidget");
             var text2 = _root.AddChild(textW2);
             text2.AddProperty(new FontSize(20));
-            WidgetTreeVisitor.ApplyPropertiesOnTree(_root);
-            Log.Debug($"Text widget @ {textWidget.Space}");
-            Log.Debug($"Text widget 2 @ {textW2.Space}");
+            var btn = Factories.Widgets.ImageButton("Sprite-0001");
+            _root.AddChild(btn);
+            _root.AddChild(Factories.Widgets.ImageButton("Sprite-0002"));
+            // WidgetTreeVisitor.ApplyPropertiesOnTree(_root);
         }
 
+        public void Resize(int width, int height)
+        {
+            _root.Data.Space = new Rectangle(_root.Data.Space.X, _root.Data.Space.Y, width, height);
+            WidgetTreeVisitor.ApplyPropertiesOnTree(_root);
+        }
+        
         public void Update()
         {
+            // Log.Debug($"_root space {_root.Data.Space}");
+            // _root.Children.ForEach(c => Log.Debug($"Space of child {c.Data} is {c.Data.Space}"));
             _widgetTreeVisitor.UpdateTree(_root);
-            // _widgetTreeVisitor.CheckHovering(_root, Mouse.GetState().Position);
         }
 
         public void Draw(SpriteBatch spriteBatch) => WidgetTreeVisitor.DrawTree(_root, spriteBatch);
